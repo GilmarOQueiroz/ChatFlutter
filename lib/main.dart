c9525d440af445b0d9814c9a4b0aca37626f4452
Gilmar Oliveira Queiroz <106493642+GilmarOQueiroz@users.noreply.github.com>
2022-07-23 02:14:43 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,32 @@
+import 'package:chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 
-  QuerySnapshot snapshot = await FirebaseFirestore.instance
-    .collection('col')
-    .getDocs();
-  snapshot.docs.forEach((d) {
-    print(d.data);
-  });
 
-  /*FirebaseFirestore.instance
-      .collection('mensagens')
-      .doc('msg2')
-      .set({
-        'texto': 'Olá',
-        'from': 'Gilmar',
-        'read': false
-  });*/
-  }
+}
+
 
 class MyApp extends StatelessWidget {
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        iconTheme: const IconThemeData(
+          color: Colors.blue,
+        )
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
+
